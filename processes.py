c2af7ff3453a6dc597650507ff40045856314b74
Jared Pochtar <jpochtar@gmail.com>
2016-03-10 20:47:39 -0500

    working

--- processes.py
+++ processes.py
@@ -12,58 +12,49 @@ def clock(ticks_per_second):
         time.sleep(max(next_tick - time.time(), 0))
         last_tick = next_tick
 
-
-# if __name__ == '__main__':
-#     for tick in clock(1):
-#         print time.time()
-#         time.sleep(0.5)
-
 class QueueBuf(object):
     def __init__(self, ipc_queue):
         self.ipc_queue = ipc_queue
         self.queue = Queue.Queue()
 
-    def get_nowait():
+    def get_nowait(self):
         while not self.ipc_queue.empty():
             self.queue.put(self.ipc_queue.get_nowait())
         return self.queue.get_nowait()
 
-    def qsize():
+    def qsize(self):
         return self.queue.qsize()
-
-#def QueueBuf(ipc_queue):
-#    return ipc_queue
-
 
 def worker(name, recv_queue, other_queue, third_queue):
     our_queue = QueueBuf(recv_queue)
     lc = 1
+    ticks_per_second = 1.0/random.randint(1, 6)
     for tick in clock(ticks_per_second):
         try:
-            recieved_value = our_queue.get_nowait()
+            (recieved_value, machine) = our_queue.get_nowait()
         except Queue.Empty:
             recieved_value = None
 
         if recieved_value != None:
             lc = max(lc, recieved_value) + 1
-            print "machine " + name + " recieved", time.time(), lc
+            print "machine " + name + " recieved", machine, recieved_value, time.time(), lc
 
         else:
             die = random.randint(1, 10)
             if die == 1:
-                other_queue.put(lc)
                 lc += 1
+                other_queue.put((lc, name))
                 print "machine " + name + " sent (1)", time.time(), lc
 
-            else if die == 2:
-                third_queue.put(lc)
+            elif die == 2:
                 lc += 1
+                third_queue.put((lc, name))
                 print "machine " + name + " sent (2)", time.time(), lc
 
-            else if die == 3:
-                other_queue.put(lc)
-                third_queue.put(lc)
+            elif die == 3:
                 lc += 1
+                other_queue.put((lc, name))
+                third_queue.put((lc, name))
                 print "machine " + name + " sent (3)", time.time(), lc
 
             else:
@@ -79,7 +70,7 @@ if __name__ == '__main__':
     qs = [multiprocessing.Queue() for i in range(3)]
     # Create three processes, and pass in the shared queues
     jobs = [multiprocessing.Process(target=worker,
-        args=tuple(str(i), q, *without(q, qs))
+        args=tuple([str(i), q] + without(q, qs)))
         for i, q in enumerate(qs)]
 
     # Start the jobs
