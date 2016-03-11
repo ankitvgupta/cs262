f089b4d41a94136f80d400b153d289d48033b8ef
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-11 00:19:20 -0500

    ran many tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,34 +1,43 @@
-# Run several jobs with default parameters
+# Run several jobs with default parameters (max_speed = 6, internal = 10, time_limit = 60)
 python processes.py
+sleep .1
 python processes.py 
+sleep .1
 python processes.py 
-python processes.py
-python processes.py
+sleep .1
+
 
 # Change prob of internal process
 python processes.py -internal 5
-python processes.py -internal 5
+sleep .1
 python processes.py -internal 15
-python processes.py -internal 15
+sleep .1
 python processes.py -internal 35
-python processes.py -internal 35
+sleep .1
+
 
 
 # Change maximum ticks per second
 python processes.py -max_speed 3
-python processes.py -max_speed 3
+sleep .1
 python processes.py -max_speed 10
-python processes.py -max_speed 10
+sleep .1
 python processes.py -max_speed 20
-python processes.py -max_speed 20
+sleep .1
 
 # Try other combinations
 python processes.py -max_speed 3 -internal 5
+sleep .1
 python processes.py -max_speed 3 -internal 15
+sleep .1
 python processes.py -max_speed 10 -internal 5
+sleep .1
 python processes.py -max_speed 10 -internal 15
+sleep .1
 python processes.py -max_speed 20 -internal 5
+sleep .1
 python processes.py -max_speed 20 -internal 15
+sleep .1
 
 
 
